e5d3db67a1165ca207148ead65273b9323a6000b
swagner210 <44910380+swagner210@users.noreply.github.com>
2019-02-13 14:57:53 -0800

    Rename prog2_3.cpp to prog2_3.hpp

---
+++ prog2_3.hpp
@@ -0,0 +1,18 @@
+#ifndef PROG2_3
+#define PROG2_3
+#include <string>
+#include <vector>
+#include <queue>
+using namespace std;
+class Parser{
+
+public:
+Parser();
+~Parser();
+
+bool Parse(vector<string> parsethis);
+
+
+};
+
+#endif
